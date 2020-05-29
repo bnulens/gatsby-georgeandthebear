02aa79bfd331feb8c71b4121bfe892120a799885
Brecht <brechtnulens@gmail.com>
2020-05-29 21:55:13 +0200

    Logo module for loading in shop Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Img from "gatsby-image"
+import styled from "styled-components"
+import { Link, useStaticQuery, graphql } from "gatsby"
+
+const LogoWrap = styled.div`
+  margin: auto 0;
+  flex: 0 1 38px;
+
+  @media (max-width: 768px) and (orientation: landscape) {
+    flex: 0 1 25px;
+  }
+`
+
+const Logo = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(name: { eq: "georgeandthebear-white" }, extension: { eq: "png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, pngQuality: 80) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <LogoWrap as={Link} to="/">
+      <Img fluid={data.file.childImageSharp.fluid} alt="logo" />
+    </LogoWrap>
+  )
+}
+
+export default Logo
