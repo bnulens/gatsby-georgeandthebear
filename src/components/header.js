8a9d9a89aac0a177a33dec4c5043fa2de96076a5
Brecht <brechtnulens@gmail.com>
2020-05-29 21:38:56 +0200

    Initial header component - header contains navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,31 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+import Navbar from "../components/navbar/Navbar"
+import Flex from "./Flex"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const Wrapper = styled.header`
+  max-width: 1080px;
+  margin: 0 auto;
+  margin-bottom: 20px;
+`
+
+const StyledDiv = styled(Flex)`
+  height: auto;
+  max-width: 960; 
+  padding: 20px 0;
+`
+
+const Header = () => (
+  <Wrapper>
+    <StyledDiv justifyContent="flex-end" alignItems="center">
+      <Navbar />
+    </StyledDiv>
+  </Wrapper>
 )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
 }
 
 export default Header
