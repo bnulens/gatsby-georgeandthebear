ef90237bff18b2d532bc812f1352b76097bca999
Brecht <brechtnulens@gmail.com>
2020-05-29 22:20:28 +0200

    Default second page is shop page - deleted starter code

---
+++ src/pages/shop.js
@@ -0,0 +1,16 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Shop = () => (
+  <Layout>
+    <SEO title="George and the Bear - Shop" />
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Shop
