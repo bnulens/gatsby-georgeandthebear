1624b792bb849c3111c25e51038282219a525525
Shutsen <vancampenhoutgeert@gmail.com>
2020-06-02 22:58:09 +0200

    Start playing with the data

--- src/templates/record-page.js
+++ src/templates/record-page.js
@@ -1,5 +1,30 @@
-const ProductPage = () => {
-    return null;
+import React from "react"
+import { graphql } from "gatsby"
+
+const RecordPage = ({ data }) => {
+  const { title, price, artist } = data.markdownRemark.frontmatter
+
+  return (
+    <>
+      <div>{title}</div>
+      <div>{price}</div>
+      <div>{artist}</div>
+    </>
+  )
 }
 
-export default ProductPage;
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        price
+        artist
+        image
+      }
+    }
+  }
+`
+
+export default RecordPage
