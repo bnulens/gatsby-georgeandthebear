6165f55f5c84e901597d5ec058c8f5c5d6121ad5
Brecht <brechtnulens@gmail.com>
2020-06-05 22:57:44 +0200

    Deleted image query - imported as svg ShopIcon

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,9 @@
 import React from "react"
-import Img from "gatsby-image"
+import ShopIcon from "../icons/svg/georgeandthebear.svg"
 import styled from "styled-components"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 
 const LogoWrap = styled.div`
-  ${'' /* margin: auto 0; */}
   flex: 0 1 38px;
 
   @media (max-width: 768px) and (orientation: landscape) {
@@ -13,24 +12,9 @@ const LogoWrap = styled.div`
 `
 
 const Logo = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      file(name: { eq: "georgeandthebear-white" }, extension: { eq: "png" }) {
-        childImageSharp {
-          fluid(maxWidth: 500, pngQuality: 100) {
-            base64
-            aspectRatio
-            src
-            srcSet
-            sizes
-          }
-        }
-      }
-    }
-  `)
   return (
     <LogoWrap as={Link} to="/">
-      <Img fluid={data.file.childImageSharp.fluid} alt="logo" />
+      <ShopIcon width="580" height="580" />
     </LogoWrap>
   )
 }
