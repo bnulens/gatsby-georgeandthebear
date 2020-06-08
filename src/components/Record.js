e0b8c916a0d6bbdcb43fe514119677835074ad99
Brecht <brechtnulens@gmail.com>
2020-06-08 22:36:07 +0200

    ImageOverlay making record info absolute to the record image

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,19 +1,48 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { Image } from 'cloudinary-react'
-import styled from 'styled-components'
+import React from "react"
+import { Link } from "gatsby"
+import { Image } from "cloudinary-react"
+import Flex from "../components/Flex"
+import styled from "styled-components"
 import ReactCountryFlag from "react-country-flag"
 
-const StyledLink = styled(Link)`
+const StyledWrapperLink = styled(Link)`
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
     flex: 0 0 auto;
+`
+const ImageOverlay = styled(Flex)`
+    position: absolute;
+    padding: 48px;
+    height: 300px;
+    width: 300px;
+    border: 1px solid red;
+    ${'' /* -webkit-box-shadow: 0px 0px 30px 3px rgba(0,0,0,0.1);
+    -moz-box-shadow: 0px 0px 30px 3px rgba(0,0,0,0.1);
+    box-shadow: 0px 0px 30px 3px rgba(0,0,0,0.1); */}
+    
     h3 {
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
+        width: 100%;
+        height: 30px;
+        margin: 0;
+    }
+    p {
+        margin-bottom: 24px;
+        font-weight: 200;
+        width: 300px;
+    }
+    span {
+        width: 300px;
+    }
+    span.emojiFlag {
+        font-size: 40px !important;
+        width: 300px;
+        margin: auto 0;
     }
 `
 
@@ -21,7 +50,7 @@ const Record = ({ record }) => {
     const { title, image, price, artist, band_origin, country_code } = record.node.frontmatter;
     const { slug } = record.node.fields;
     return (
-        <StyledLink to={slug}>
+        <StyledWrapperLink to={slug}>
             <Image
                 cloudName="bnulens"
                 publicId={image}
@@ -29,15 +58,17 @@ const Record = ({ record }) => {
                 height="300"
                 secure="true"
             />
-            <h3>{title}</h3>
-            <p>{artist}</p>
-            <p>€ {price}</p>
-            <ReactCountryFlag
-                className="emojiFlag"
-                countryCode={country_code}
-                aria-label={band_origin}
-            />
-        </StyledLink>
+            <ImageOverlay flexDirection="column" justifyContent="space-around">
+                <h3>{title}</h3>
+                <p>{artist}</p>
+                <span>€ {price}</span>
+                <ReactCountryFlag
+                    className="emojiFlag"
+                    countryCode={country_code}
+                    aria-label={band_origin}
+                />
+            </ImageOverlay>
+        </StyledWrapperLink>
     )
 }
 
